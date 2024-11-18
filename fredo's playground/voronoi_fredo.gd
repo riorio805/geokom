4efd6655877e10acaa6ec3e478c28e71682e101a
RobertBenyamin <robertbenyamino@gmail.com>
2024-11-18 22:25:33 +0700

    delete unneccessary comments

--- fredo's playground/voronoi_fredo.gd
+++ fredo's playground/voronoi_fredo.gd
@@ -36,17 +36,8 @@ func _draw() -> void:
 	
 	# draw edges
 	for edge in draw_edges:
-		#edge[0].y = clamp(edge[0].y, -1000, 5000)
-		#edge[1].y = clamp(edge[1].y, -1000, 5000)
-		
-		#draw_line(edge[0], edge[1], line_color, line_width, true)
 		draw_polyline(edge, voronoi_color, line_width, true)
 	for edge in draw_sketch_edges:
-		#edge[0].y = clamp(edge[0].y, -1000, 5000)
-		#edge[1].y = clamp(edge[1].y, -1000, 5000)
-		
-		#draw_line(edge[0], edge[1], line_color, line_width, true)
-		#draw_polyline(edge, circle_event_color, line_width, true)
 		pass
 		
 	if len(draw_circles) > 0:
@@ -57,9 +48,6 @@ func _draw() -> void:
 		for c in draw_circles:
 			if c[1] == max_radius:
 				draw_circle(c[0], c[1], circle_event_color, false, line_width, true)
-		
-	#print(draw_edges)
-		
 
 func _process(_delta) -> void:
 	if Input.is_action_pressed("debug") and Input.is_action_just_pressed("debug_color"):
@@ -78,15 +66,12 @@ func update_with_points(nodes:Array):
 		new_heap_node.is_site_event = true
 		new_heap_node.idx_point = i
 		min_heap.insert(new_heap_node)
-	# print(len(points))
-	
-	
+
 	var beachlines := AVLTreeFredo.new()
 	
 	beachlines.add_circle_event.connect(add_circle_event)
 	beachlines.add_edge.connect(add_edge)
 	beachlines.add_circle.connect(add_circle)
-	
 	
 	draw_edges = []
 	draw_sketch_edges = []
@@ -97,27 +82,17 @@ func update_with_points(nodes:Array):
 		var heap_node : HeapNode = min_heap.pop()
 		
 		if heap_node.is_site_event:
-			# if site event
-			# replace that (arc0) to (arc0,edge1,arc1,edge1,arc0)
-			print("Site event")
-			
-			# make new arc
+			# if site event replace that (arc0) to (arc0,edge1,arc1,edge1,arc0) make new arc
 			var p = points[heap_node.idx_point]
 			
 			beachlines.site_event(p, heap_node.priority)
 			
 		else:
-			# if circle event
-			# (edge0,arc0,edge0) -> (edge1)
-			print("Circle event")
-			
+			# if circle event (edge0,arc0,edge0) -> (edge1)
 			# draw line
 			draw_sketch_edges.append([Vector2(MIN_X, heap_node.priority), Vector2(MAX_X, heap_node.priority)])
-			# add_edge(Vector2(0, heap_node.priority), Vector2(1500, heap_node.priority))
 			beachlines.remove2(heap_node.left_focus, heap_node.middle_node, heap_node.right_focus, heap_node.priority, heap_node.intersection_point)
-			
-			
-		#beachlines.debug()
+
 		beachlines.debug2(heap_node.priority)
 		
 	# min heap habis tapi heap masih sisa beachlines
@@ -137,13 +112,9 @@ func update_with_points(nodes:Array):
 			add_edge(start, start + 10000 * Vector2(cos(dir), -sin(dir)))
 			tmp = tmp.next
 	
-	#beachlines.free()
 	queue_redraw()
-	print("- - - - DONE - - - -")
 
 func add_circle_event(y_sweepline, middle_arc:AVLNode, intersection_point:Vector2):
-	print("- add circle event")
-	
 	var circle_event = HeapNode.new()
 	
 	circle_event.priority = y_sweepline
