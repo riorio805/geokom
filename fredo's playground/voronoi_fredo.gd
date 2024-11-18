395a2e33bc139b3522f9375713fe250352c0e434
RobertBenyamin <robertbenyamino@gmail.com>
2024-11-18 22:27:59 +0700

    give more explanation in comments

--- fredo's playground/voronoi_fredo.gd
+++ fredo's playground/voronoi_fredo.gd
@@ -23,6 +23,7 @@ static var MAX_X = 2000
 
 var bounding_box:Rect2 = Rect2()
 
+# Update camera bounds and adjust MIN_X, MAX_X, MIN_Y, MAX_Y accordingly
 func update_camera(bounds:Rect2):
 	bounding_box = bounds
 	MIN_X = bounding_box.position.x
@@ -30,6 +31,7 @@ func update_camera(bounds:Rect2):
 	MIN_Y = bounding_box.position.y
 	MAX_Y = bounding_box.end.y
 
+# Draw the Voronoi diagram
 func _draw() -> void:
 	# draw boundary
 	draw_polyline([Vector2(MIN_X, MIN_Y), Vector2(MIN_X, MAX_Y), Vector2(MAX_X, MAX_Y), Vector2(MAX_X, MIN_Y), Vector2(MIN_X, MIN_Y)], voronoi_color, line_width, true)
@@ -40,6 +42,7 @@ func _draw() -> void:
 	for edge in draw_sketch_edges:
 		pass
 		
+	# draw circles with the largest radius
 	if len(draw_circles) > 0:
 		var max_radius = draw_circles[0][1]
 		for c in draw_circles:
@@ -49,11 +52,13 @@ func _draw() -> void:
 			if c[1] == max_radius:
 				draw_circle(c[0], c[1], circle_event_color, false, line_width, true)
 
+# Process input and redraw if necessary
 func _process(_delta) -> void:
 	if Input.is_action_pressed("debug") and Input.is_action_just_pressed("debug_color"):
 		pass
 		queue_redraw()
 
+# Update the diagram with new points
 func update_with_points(nodes:Array):
 	# get positions of nodes
 	var points := nodes.map(func (p): return p.position)
@@ -69,6 +74,7 @@ func update_with_points(nodes:Array):
 
 	var beachlines := AVLTreeFredo.new()
 	
+	# Connect signals for circle events and edges
 	beachlines.add_circle_event.connect(add_circle_event)
 	beachlines.add_edge.connect(add_edge)
 	beachlines.add_circle.connect(add_circle)
@@ -78,31 +84,29 @@ func update_with_points(nodes:Array):
 	draw_circles = []
 	queue_redraw()
 	
+	# Process events from the min heap
 	while !min_heap.is_empty():
 		var heap_node : HeapNode = min_heap.pop()
 		
 		if heap_node.is_site_event:
-			# if site event replace that (arc0) to (arc0,edge1,arc1,edge1,arc0) make new arc
+			# Handle site event
 			var p = points[heap_node.idx_point]
-			
 			beachlines.site_event(p, heap_node.priority)
-			
 		else:
-			# if circle event (edge0,arc0,edge0) -> (edge1)
-			# draw line
+			# Handle circle event
 			draw_sketch_edges.append([Vector2(MIN_X, heap_node.priority), Vector2(MAX_X, heap_node.priority)])
 			beachlines.remove2(heap_node.left_focus, heap_node.middle_node, heap_node.right_focus, heap_node.priority, heap_node.intersection_point)
 
 		beachlines.debug2(heap_node.priority)
 		
-	# min heap habis tapi heap masih sisa beachlines
+	# Process remaining beachlines
 	var tmp = beachlines.root
 	if tmp != null:
 		while (tmp.prev != null):
 			tmp = tmp.prev
 			
 		while (tmp.next != null): # karena arc terakhir tidak ada edge dikanannya
-			# tmp ini dipake buat draw edge
+			# Draw remaining edges
 			var start = tmp.right_edge_start
 			var dir = tmp.right_edge_direction
 			if start.y < MIN_Y:
@@ -114,6 +118,7 @@ func update_with_points(nodes:Array):
 	
 	queue_redraw()
 
+# Add a circle event to the min heap
 func add_circle_event(y_sweepline, middle_arc:AVLNode, intersection_point:Vector2):
 	var circle_event = HeapNode.new()
 	
@@ -126,18 +131,21 @@ func add_circle_event(y_sweepline, middle_arc:AVLNode, intersection_point:Vector
 	
 	min_heap.insert(circle_event)
 
+# Add an edge to the list of edges to be drawn
 func add_edge(from:Vector2, to:Vector2):
 	draw_edges.append([from, to])
 	queue_redraw()
 
+# Add a circle to the list of circles to be drawn
 func add_circle(center, radius):
 	draw_circles.append([center, radius])
 	queue_redraw()
 	
-# sort by y-value ascending
+# Sort by y-value ascending
 func compare_y(a:Vector2, b:Vector2):
 	return a.y<b.y
 
+# Calculate the angle between three points
 ## Positive if turn left, negative if turn right
 func angle_between_points(p1:Vector2, p2:Vector2, p3:Vector2):
 	# https://stackoverflow.com/a/2150475
